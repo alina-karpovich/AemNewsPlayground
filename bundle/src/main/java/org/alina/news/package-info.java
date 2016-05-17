f433b03a3cfcdd79ca8487de0e75a1f2de176884
Alina Karpovich <karpovich.alina@gmail.com>
2016-05-18 01:05:19 +0300

    Initital commit.

---
+++ bundle/src/main/java/org/alina/news/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package org.alina.news;
+
+import aQute.bnd.annotation.Version;
---
+++ bundle/src/test/java/org/alina/news/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package org.alina.news;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
